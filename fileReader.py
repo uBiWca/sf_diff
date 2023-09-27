2100b315cf4dce0bb95c491c7c62f234ffa9355c
Serge Kazachenko <sergey.kazachenko@succraft.com>
2023-09-27 23:23:58 +0300

    Defined variables and constants

--- fileReader.py
+++ fileReader.py
@@ -1,17 +1,26 @@
 import os
 class FileListBuilder(object):
-    dirList = []
+    __dirList__ = None
+    apexClassesFolderPath = None
+    lwcComponentsFolderPath = None
+    auraComponentsFolderPath = None
+    MESSAGE_FORCEAPP_FOLDER_NOT_FOUND = "force-app folder not found in a given folder"
     def __init__(self, projectPath):
         self.__path__=projectPath
         self.__makeFileList()
         
-    def __makeFileList(self):
-        obj = os.scandir(self.__path__)
-        for entry in obj:
-            if (entry.is_dir):
-                self.dirList.append(entry.path)
+    def __makeFileList(self):        
+        for entry in os.scandir(self.__path__):
+            if entry.is_dir and entry.path.endswith("force-app"):
+                self.__dirList__ = entry.path
+        if self.__dirList__ == None:
+            self.__dirList__ = self.MESSAGE_FORCEAPP_FOLDER_NOT_FOUND
+        else:
+            os.chdir(self.__dirList__+"\\main\\default")
+            for entry in os.scandir(self.__dirList__+"\\main\\default"):
+                print(entry.path)
         # for (root, dirs, file) in os.walk(self.__path__):
         #     for dir in dirs:
         #         self.dirList.append(dir)
     def printList(self):
-        print (self.dirList)
+        print (self.__dirList__)
