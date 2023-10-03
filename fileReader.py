bb3aea7c02be04758acb5503bcc27f428f551efd
Serge Kazachenko <sergey.kazachenko@succraft.com>
2023-10-03 22:40:43 +0300

    Added some basic search for omponents/classes

--- fileReader.py
+++ fileReader.py
@@ -1,15 +1,15 @@
 import os
+import re
 class FileListBuilder(object):
     __dirList__ = None
     __apexClassesFolderPath__ = None
     __lwcComponentsFolderPath__ = None
-    __auraComponentsFolderPath__ = None
-    __lwcComponentsList__= []
-    __lwcCompnentsPathsMap__ = {}
-    __auraComponentsList__= []
-    __auraComponentsPathsMap__ = {}
-    __apexClassesList__= []
-    APEX_REGEX_PATTERN = "\\.+\.cls$"
+    __auraComponentsFolderPath__ = None    
+    __apexClasses__ = []
+    __auraComponents__ = []
+    __lwcComponents__ = []
+    APEX_REGEX_PATTERN = "[\w\d\s]*\.cls$"
+    COMPONENTS_REGEX_PATTERN ="[\w\d\s]*$"
     MESSAGE_FORCEAPP_FOLDER_NOT_FOUND = "force-app folder not found in a given folder"
     def __init__(self, projectPath):
         self.__path__=projectPath
@@ -17,7 +17,7 @@ class FileListBuilder(object):
         
     def __makeFileList(self):        
         for entry in os.scandir(self.__path__):
-            if entry.is_dir and entry.path.endswith("force-app"):
+            if entry.is_dir() and entry.path.endswith("force-app"):
                 self.__dirList__ = entry.path
         if self.__dirList__ == None:
             self.__dirList__ = self.MESSAGE_FORCEAPP_FOLDER_NOT_FOUND
@@ -36,28 +36,45 @@ class FileListBuilder(object):
             self.__getAuraComponentsList()
         if self.__apexClassesFolderPath__:
             self.__getApexClassesList()            
-        # for (root, dirs, file) in os.walk(self.__path__)
-        #     for dir in dirs:
-        #         self.dirList.append(dir)
+        
     def __getLWCComponentsList(self):
         for entry in os.scandir(self.__lwcComponentsFolderPath__):
-            if entry.is_dir:
-                self.__lwcComponentsList__.append(entry.path)
-                for componentElement in os.scandir(entry.path):
-                    if not componentElement.path.endswith("xml") and not componentElement.is_dir:
-                        self.__lwcCompnentsPathsMap__[componentElement.path.s]
+            if entry.is_dir():                
+                self.__getClassOrComponentName(entry.path, False, False)
+               
     def __getApexClassesList(self):
-        for entry in os.scandir(self.__apexClassesFolderPath__):
-            if entry.is_dir and not entry.path.endswith("xml"):
-                self.__apexClassesList__.append(entry.path)
+        for entry in os.scandir(self.__apexClassesFolderPath__):            
+            if entry.is_file() and not entry.path.endswith("xml"):
+                self.__getClassOrComponentName(entry.path, True, False)
+                
     def __getAuraComponentsList(self):
         for entry in os.scandir(self.__auraComponentsFolderPath__):
-            if entry.is_dir:
-                self.__auraComponentsList__.append(entry.path)
+            if entry.is_file():
+                self.__getClassOrComponentName(entry.path, False, True)
+
+    # Checks end of the path line to determine is this path of apex class or a component, 
+    # extracts it name and adds it to one of lists
+    def __getClassOrComponentName(self, path, isClass, isAura):        
+        regex = (re.compile(self.COMPONENTS_REGEX_PATTERN),re.compile(self.APEX_REGEX_PATTERN))[isClass]
+        match = regex.search(path)        
+        if match:            
+            if isClass:
+                 self.__apexClasses__.append(match.group(0)[:-4])
+            elif not isClass and isAura:
+                self.__auraComponents__.append(match.group(0))
+            elif not isClass and not isAura:
+                self.__lwcComponents__.append(match.group(0))
+
+    def getAuraComponents(self):
+        return self.__auraComponents__
+
+    def getLWCComponents(self):
+        return self.__lwcComponents__
+
+    def getApexClasses(self):
+        return self.__apexClasses__
+
     def printList(self):
-        # print (self.__apexClassesFolderPath__)
-        # print (self.__lwcComponentsFolderPath__)
-        # print (self.__auraComponentsFolderPath__)
-        # print (self.__lwcComponentsList__)
-        # print (self.__auraComponentsList__)
-        print (self.__apexClassesList__)
+        
+        print (self.__apexClasses__)
+        print (self.__lwcComponents__)
