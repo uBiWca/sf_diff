f72751d2abcabef574dc2a48576a2d29ca72d4ee
Serge Kazachenko <sergey.kazachenko@succraft.com>
2023-10-16 23:24:33 +0300

    Fixed custom object regex, started to work on cliHandler

--- apexClassParser.py
+++ apexClassParser.py
@@ -1,22 +1,43 @@
 import fileReader
 import re
+
+
 class ApexParser(object):
-    CUSTOM_OBJECT_NAME_REGEX = "\w*__c|__C"
+    CUSTOM_OBJECT_NAME_REGEX = "(\(|\(\s*|[new\s*])(\w*__c|__C)(?=\s)"
     CUSTOM_FIELD_NAME_REGEX = ".\w*__c|__C"
     __apexClasses__ = []
     __apexClassesPaths__ = {}
     __fieldsNames__ = []
+
     def __init__(self, fileReader):
         self.__apexClasses__ = fileReader.getApexClasses()
         self.__apexClassesPaths = fileReader.getApexClassPath()
-    #Parses class content and extracts all custom fields names
+        self.__parseFiles__()
+    # Iterates over classes list and extracts all custom fields names and custom objects names
+
+    def __parseFiles__(self):
+        if self.__apexClassesPaths__:
+            for apexClass in self.__apexClassesPaths__.values():
+                file = open(apexClass, 'r')
+                self.__searchClassForFields__(file)
+    # Parses class content and extracts all custom objects names
+
+    def __searchClassForObjects__(self, classContent):
+        regex = re.compile(self.CUSTOM_OBJECT_NAME_REGEX)
+        result = regex.findall(classContent)
+        if result:
+            for objectName in result:
+                strippedName = fieldNamde[1:]
+                if strippedName not in self.__fieldsNames__:
+                    self.__fieldsNames__.append(strippedName)
+    # Parses class content and extracts all custom fields names
     #
+
     def __searchClassForFields__(self, classContent):
         regex = re.compile(self.CUSTOM_OBJECT_NAME_REGEX)
-        result = regex.findall(classContent) 
+        result = regex.findall(classContent)
         if result:
             for fieldNamde in result:
                 strippedName = fieldNamde[1:]
                 if strippedName not in self.__fieldsNames__:
                     self.__fieldsNames__.append(strippedName)
-    
--- cliHandling.py
+++ cliHandling.py
@@ -1,1 +1,9 @@
 import argparse
+class CliHandling(object):
+    __parser__ = None
+    def __init__(self):
+        self.__parser__ =argparse.ArgumentParser(description="""This app is used to analize Salesforce projetc content 
+        and to compare it against target Org, in order to find custom objects and fields not existing in target Org, but
+        referenced in project. It also can generate package.xml file, containing all differences found and compare classes
+        and components content in order to determine, which of them needs to be deployed.""")
+        self.__parser__.add_argument(['--projectPath', '--path', 'p'], action='store', help="""Path to \'main\' folder.""")
